ef600598f3f3250fa866a8674c545c67195d355a
Hızır Ceylan <119620935+hizirceylan@users.noreply.github.com>
2024-03-07 13:23:38 +0300

    Adding boosters and tags
    Adding boosters and making the car slow down when bump into another object

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -7,11 +7,8 @@ public class Driver : MonoBehaviour
 
     [SerializeField]float steerSpeed = 1f;
     [SerializeField] float moveSpeed = 0.01f;
-
-    void Start()
-    {
-        
-    }
+    [SerializeField] float slowSpeed = 5f;
+    [SerializeField] float boostSpeed = 20f;
 
     void Update()
     {
@@ -19,5 +16,19 @@ public class Driver : MonoBehaviour
        float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
        transform.Rotate(0, 0, -steerAmount);
        transform.Translate(0, moveAmount, 0);
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) 
+    {
+        moveSpeed = slowSpeed;    
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Boost")
+        {
+            moveSpeed = boostSpeed;
+        }    
     }
 }
